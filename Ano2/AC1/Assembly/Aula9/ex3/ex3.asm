2a880d1b06df7508e9af00141ddc3ee0228efa51
tiagooliveira95 <t.oliveira.95@gmail.com>
2021-01-31 01:41:58 +0000

    Revert "minor correction"
    This reverts commit ebda73b332913dc523a3459455dd9d5784243e26.

--- Ano2/AC1/Assembly/Aula9/ex3/ex3.asm
+++ Ano2/AC1/Assembly/Aula9/ex3/ex3.asm
@@ -10,29 +10,28 @@ a:	.space 80
 #	$t1 > &a[i]
 #	$t2-> a
 main:	addiu	$sp,$sp, -4
-		sw	$ra,0($sp)
-		li	$t0,0			# i = 0	
-		la	$t2,a
-for:		bge	$t0,SIZE,endfor		# for(i = 0; i < SIZE; i++) {
-		li	$v0, read_int
+		sw		$ra,0($sp)
+		li		$t0,0			# i = 0	
+		la		$t2,a
+for:		bge		$t0,SIZE,endfor	# for(i = 0; i < SIZE; i++) {
+		li		$v0, read_int
 		syscall
 		mtc1	$v0,$f12
-		cvt.d.w	$f12,$f12		# (double)read_int();
-		sll	$t1,$t0,3		# i  * 8
+		cvt.d.w	$f12,$f12			# (double)read_int();
+		sll		$t1,$t0,3			# i  * 8
 		addu	$t1,$t1,$t2
-		s.d	$f12,0($t1)		#a[i] = (double)read_int();		
-		addiu	$t0,$t0,1		#i++
-		j 	for
+		s.d		$f12,0($t1)		#a[i] = (double)read_int();		
+		addiu	$t0,$t0,1			#i++
+		j 		for
 endfor:	move	$a0,$t2
-		li	$a1,SIZE
-		jal	average
-		mov.d	$f12,$f0
-		li	$v0, print_double
+		li		$a1,SIZE
+		jal		average
+		li		$v0, print_double
 		syscall
-		li	$v0,0			#return 0;
-		lw	$ra,0($sp)
+		li		$v0,0			#return 0;
+		lw		$ra,0($sp)
 		addiu	$sp,$sp, 4
-		jr	$ra
+		jr		$ra
 		
 
 
