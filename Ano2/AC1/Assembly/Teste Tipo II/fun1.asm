23b6ae5b7474c78f9584355bd9cd8ce9744473e2
Tiago Oliveira <t.oliveira.95@gmail.com>
2021-01-31 23:53:01 +0000

    fix format

--- Ano2/AC1/Assembly/Teste Tipo II/fun1.asm
+++ Ano2/AC1/Assembly/Teste Tipo II/fun1.asm
@@ -24,12 +24,12 @@ fun1:		move	$t0,$a0			# t0 = a
 for:		bge	$t4,$t5,endfor		#  for(; p < (a + N); p++) {
 		l.d	$f4,0($t4)		# f4 = * p
 		l.d	$f8,x			# f8 = 2.0
-		div.d $f8,$f4,$f8		#f8 = *p / 2.0
+		div.d 	$f8,$f4,$f8		#f8 = *p / 2.0
 		c.eq.d	$f8,$f10		# if ( (*p / 2.0) != 0.0 ) 
 		bc1t	else			#{
 		s.d	$f4,0($t2)		# *b++ = *p
 		addi	$t2,$t2,8		# b++
-		j endif				#} else {
+		j 	endif			#} else {
 else:		addi	$t3,$t3,1		# k++ }
 endif:		addi	$t4,$t4,8		# p++
 		j	for			#}
