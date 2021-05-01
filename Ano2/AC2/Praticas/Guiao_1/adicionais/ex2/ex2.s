dbd3c0770c119746959d1186842fb5b24d94172b
tiagooliveira95 <t.oliveira.95@gmail.com>
2021-05-01 23:32:30 +0100

    ex2 incompleto

---
+++ Ano2/AC2/Praticas/Guiao_1/adicionais/ex2/ex2.s
@@ -0,0 +1,31 @@
+        .equ SRT_MAX_SIZE, 20
+        .equ printStr, 8
+        .equ readStr, 9
+        .equ printInt, 6
+        .equ printInt10, 7
+        .data
+s0:     .asciiz "Introduza 2 strings: "
+s1:     .asciiz "Resultados:\n"
+str1:   .space 21                       # char str1[STR_MAX_SIZE + 1]
+str2:   .space 21                       # char str2[STR_MAX_SIZE + 1]
+str3:   .space 41                       # char str3[2 * STR_MAX_SIZE + 1]
+        .text
+        .globl main
+
+main:   li $v0, printStr
+        la $a0, s0
+        syscall                         # printStr("Introduza 2 strings: ");
+
+        li $v0, readStr
+        la $a0, str1
+        li $a1, STR_MAX_SIZE
+        syscall                         # readStr( str1, STR_MAX_SIZE );
+
+        li $v0, readStr
+        la $a0, str2
+        li $a1, STR_MAX_SIZE
+        syscall                         #  readStr( str2, STR_MAX_SIZE );
+
+        li $v0, printStr
+        la $a0, s1
+        syscall                         #  printStr("Resultados:\n");
