f19f60c9249e5bb15733f7c3a23a820330fbe4ef
tiagooliveira95 <t.oliveira.95@gmail.com>
2021-05-02 18:20:13 +0100

    format improved

--- Ano2/AC2/Praticas/Guiao_2/ex4/ex4.s
+++ Ano2/AC2/Praticas/Guiao_2/ex4/ex4.s
@@ -15,54 +15,54 @@ main:   addiu   $sp,$sp,-16
         sw      $s0,4($sp)
         sw      $s1,8($sp)
         sw      $s2,12($sp)
-        li      $s0, 0          # cnt1 = 0;
-        li      $s1, 0          # cnt5 = 0;
-        li      $s2, 0          # cnt10 = 0;
-while:  li      $a0,100         # while(1) {
-        jal     delay           #   delay(100)
+        li      $s0, 0                      # cnt1 = 0;
+        li      $s1, 0                      # cnt5 = 0;
+        li      $s2, 0                      # cnt10 = 0;
+while:  li      $a0,100                     # while(1) {
+        jal     delay                       #   delay(100)
 
-        addi    $s2,$s2,1       #   cnt10++
+        addi    $s2,$s2,1                   #   cnt10++
 
-        rem     $t0,$s2,2       #   t0 = cnt % 2
-        seq     $t0,$t0,$0      #   t0 = cnt10 & 2 == 0
-        add     $s1,$s1,$t0     #   cnt5 += cnt10 % 2 == 0;
+        rem     $t0,$s2,2                   #   t0 = cnt % 2
+        seq     $t0,$t0,$0                  #   t0 = cnt10 & 2 == 0
+        add     $s1,$s1,$t0                 #   cnt5 += cnt10 % 2 == 0;
 
-        rem     $t0,$s2,10      #   t0 = cnt % 10
-        seq     $t0,$t0,$0      #   t0 = cnt10 & 10 == 0
-        add     $s0,$s0,$t0     #   cnt1 += cnt10 % 10 == 0;
+        rem     $t0,$s2,10                  #   t0 = cnt % 10
+        seq     $t0,$t0,$0                  #   t0 = cnt10 & 10 == 0
+        add     $s0,$s0,$t0                 #   cnt1 += cnt10 % 10 == 0;
 
         li      $a0, '\r'
         li      $v0, putChar
-        syscall                 #   putChar('\r');
+        syscall                             #   putChar('\r');
         li      $v0,printInt
-        move    $a0,$s0         #   a0 = cnt1
-        li      $a1,0x5000A     #   a1 = 10 | 5 << 16
-        syscall                 #   printInt(cnt1, 10 | 5 << 16);
+        move    $a0,$s0                     #   a0 = cnt1
+        li      $a1,0x5000A                 #   a1 = 10 | 5 << 16
+        syscall                             #   printInt(cnt1, 10 | 5 << 16);
 
         li      $a0, '\t'
         li      $v0, putChar
-        syscall                 #   putChar('\t');
+        syscall                             #   putChar('\t');
         li      $v0,printInt
-        move    $a0,$s1         #   a0 = cnt5
-        li      $a1,0x5000A     #   a1 = 10 | 5 << 16
-        syscall                 #   printInt(cnt5, 10 | 5 << 16);
+        move    $a0,$s1                     #   a0 = cnt5
+        li      $a1,0x5000A                 #   a1 = 10 | 5 << 16
+        syscall                             #   printInt(cnt5, 10 | 5 << 16);
 
         li      $a0, '\t'
         li      $v0, putChar
-        syscall                  #  putChar('\t');
+        syscall                             #  putChar('\t');
         li      $v0,printInt
-        move    $a0,$s2          #  a0 = cnt10
-        li      $a1,0x5000A #   a1 = 10 | 5 << 16
-        syscall                  #  printInt(cnt10, 10 | 5 << 16);
+        move    $a0,$s2                     #  a0 = cnt10
+        li      $a1,0x5000A                 #   a1 = 10 | 5 << 16
+        syscall                             #  printInt(cnt10, 10 | 5 << 16);
 
-        j   while                # }
+        j   while                           # }
 
         lw      $ra,0($sp)
         lw      $s0,4($sp)
         lw      $s1,8($sp)
         lw      $s2,12($sp)
         addiu   $sp,$sp,16
-        li      $v0,0             # return 0
+        li      $v0,0                       # return 0
         jr      $ra
 
 delay:  ble     $a0,0, endFor               # for(; ms > 0; ms--) {
