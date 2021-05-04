aebf9a8d6116334bcc1b6b0341f03c5e8ad7be28
tiagooliveira95 <t.oliveira.95@gmail.com>
2021-05-04 02:51:01 +0100

    ex2

---
+++ Ano2/AC2/Praticas/Guiao_4/Parte1/ex2/ex2.c
@@ -0,0 +1,23 @@
+#include "/opt/pic32mx/include/detpic32.h"
+
+void delay(int);
+
+int main(void){
+    TRISE = TRISE & 0xFFF0;
+    LATE = LATE & 0xFFF0;
+
+    int counter = 0;
+    while(1){
+        delay(200);
+        LATE = (LATE & 0xFFF0) | counter++;
+        if(counter == 0xF) counter = 0;
+    }
+    return 0;
+}
+
+void delay(int ms){
+    for(; ms > 0; ms--) {
+        resetCoreTimer();
+        while(readCoreTimer() < 20000);
+    }
+}
