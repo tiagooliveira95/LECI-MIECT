7fab278242e9e7f57f9980aaf2935cf284e7a2e4
tiagooliveira95 <t.oliveira.95@gmail.com>
2021-05-05 00:18:24 +0100

    ex2

---
+++ Ano2/AC2/Praticas/Exame_Prático_Modelo_1/ex2/ex2.c
@@ -0,0 +1,55 @@
+#include <detpic32.h>
+
+void display(char value);
+void delay(int ms);
+
+int main(void){
+    TRISB = TRISB && 0x80FF | 0xF;
+    TRISD = TRISD & 0xFF9F;
+
+    int i;
+    int counter = 0;
+    while(1){
+        char c = getChar();
+        i = 0;
+
+        if(c >= '0' && c <= '3'){
+            do{
+                display(c-0x30);
+                delay(10);
+            }while(++i < 20);
+
+        }else{
+            do{
+                display(254);
+                delay(10);
+            }while(++i < 100);
+        }
+        LATD = (LATD & 0x9F);
+    }
+}
+
+void display(char value){
+    char segmentCodes[] =  {
+            0x3F,0x06,0x5B,0x4F,0x66,0x6D,0x7D,0x07,0x7F,0x6F,0x77,0x7C,0x39,0x5E,0x79,0x71
+    };
+
+    static char flag = 1;
+
+    if(flag == 1){
+        LATD = (LATD & 0x9F) | 0x40;
+        int temp = value >> 4;
+        LATB = (LATB & 0x80FF) | segmentCodes[value >> 4] << 8;
+    } else {
+        LATD = (LATD & 0x9F) | 0x20;
+        LATB = (LATB & 0x80FF) | segmentCodes[value & 0x0F] << 8;
+    }
+    flag = !flag;
+}
+
+void delay(int ms) {
+    for (; ms > 0; ms--) {
+        resetCoreTimer();
+        while (readCoreTimer() < 20000);
+    }
+}
