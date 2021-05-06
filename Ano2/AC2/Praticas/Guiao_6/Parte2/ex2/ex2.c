be4975666502c3f91d5b965079ece777fc67d80a
tiagooliveira95 <t.oliveira.95@gmail.com>
2021-05-06 19:01:42 +0100

    ex2

---
+++ Ano2/AC2/Praticas/Guiao_6/Parte2/ex2/ex2.c
@@ -0,0 +1,44 @@
+#include<detpic32.h>
+
+volatile int adc_value;
+
+/*
+ * Para a placa DETPIC32-IO foi usado o testpoint OC1 em vez do RE0
+ */
+
+// Interrupt Handler
+void _int_(27) isr_adc(void){ // Replace VECTOR by the A/D vector number - see "PIC32 family data sheet" (pages 74-76)
+    adc_value = ADC1BUF0;
+    AD1CON1bits.ASAM = 1;                  // Start conversion
+    IFS1bits.AD1IF = 0;                    // Reset AD1IF flag
+    LATDbits.LATD0 = 1;                    // OC1 = 1
+}
+
+int main(void) {
+    TRISDbits.TRISD0 = 0;
+
+    // Configure A/D module
+    TRISBbits.TRISB4 = 1;
+    AD1PCFGbits.PCFG4 = 0;
+    AD1CON1bits.SSRC = 7;
+    AD1CON1bits.CLRASAM = 1;
+    AD1CON3bits.SAMC = 16;
+    AD1CON2bits.SMPI = 1 - 1;
+    AD1CHSbits.CH0SA = 4;
+    AD1CON1bits.ON = 1;
+
+    // Configure interrupt system
+    IPC6bits.AD1IP = 2;  // configure priority of A/D interrupts
+    IFS1bits.AD1IF = 0;  // clear A/D interrupt flag
+    IEC1bits.AD1IE = 1;  // enable A/D interrupts
+    EnableInterrupts(); // Global Interrupt Enabled
+
+
+    AD1CON1bits.ASAM = 1;           // Start conversion
+
+    while (1){
+        LATDbits.LATD0 = 0;                    // OC1 = 0
+    }
+
+    return 0;
+}
