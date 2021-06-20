8da49ff47bccf5ea441d09ba43c84bcdc974397f
tiagooliveira95 <t.oliveira.95@gmail.com>
2021-06-20 16:57:22 +0100

    ex2 feito :)

--- Ano2/AC2/Praticas/Guiao_7/Parte1/ex2/ex2.c
+++ Ano2/AC2/Praticas/Guiao_7/Parte1/ex2/ex2.c
@@ -1,22 +1,25 @@
 #include <detpic32.h>
+void configIO();
 
-
-void main(void)
-{
-    T3CONbits.TCKPS = 7; // 1:256 prescaler (i.e Fout_presc = 78125 KHz)
-    PR3 = 39063; // Fout = 20MHz / (32 * (39063 + 1)) = 1.999 Hz
-    TMR3 = 0; // Reset timer T3 count register
-    T3CONbits.TON = 1; // Enable timer T3 (must be the last command of the
-
-    IPC3bits.T3IP = 2;      // Interrupt priority 2
-    IEC0bits.T3IE = 1;      // Enable timer T3 interrupts
-    IFS0bits.T3IF = 0;      // Reset timer T3 interrupt flag
-
-    // Configure Timer T3 with interrupts enabled
+void main(void) {
+    configIO();
+    // Configure Timer T3 with inte brrupts enabled
     EnableInterrupts();
     while(1);
 }
 
+void configIO() {
+    T3CONbits.TCKPS = 7; // define o Prescaler ara dividir o clock por 256
+    // fOut_PRESC = 20Mhz / 256 = 78125Hz
+    // PR3 = (78125Hz / 2 Hz) + 1 = 39061.5
+    PR3 = 39063; // Fout = 20MHz / (32 * (39063 + 1)) = 1.999 Hz
+    TMR3 = 0; // Reset timer T3 count register
+    T3CONbits.TON = 1; // Enable timer T3 (must be the last command)
+
+    IFS0bits.T3IF = 0;      // Reset timer T3 interrupt flag, should be set before enabling interrupts
+    IPC3bits.T3IP = 2;      // Interrupt priority 2
+    IEC0bits.T3IE = 1;      // Enable timer T3 interrupts
+}
 
 void _int_(12) isr_T3(void) // Replace VECTOR by the timer T3
 // vector number
