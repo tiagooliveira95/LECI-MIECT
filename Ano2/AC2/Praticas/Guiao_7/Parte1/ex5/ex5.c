f9e6123642ba9b870cbdc31214280769ee0b5750
tiagooliveira95 <t.oliveira.95@gmail.com>
2021-06-22 01:26:29 +0100

    ex5

---
+++ Ano2/AC2/Praticas/Guiao_7/Parte1/ex5/ex5.c
@@ -0,0 +1,134 @@
+#include <detpic32.h>
+
+volatile int voltage = 0; // Global variable
+
+void configureAll();
+void send2displays(unsigned char);
+unsigned char toBcd(unsigned char);
+
+void _int_(12) isr_T3(void) { // adc_print
+    // Send "voltage" global variable to displays
+    send2displays(voltage);
+    // Reset T3 interrupt flag
+    IFS0bits.T3IF = 0;
+}
+
+void _int_(4) isr_T1(void) { // adc_convert
+    // Start A/D conversion
+    AD1CON1bits.ASAM = 1;           // Start conversion
+    // Reset T1 interrupt flag
+    IFS0bits.T1IF = 0;
+}
+
+void _int_(27) isr_adc(void){ // Replace VECTOR by the A/D vector number - see "PIC32 family data sheet" (pages 74-76)
+    int *p = (int *)(&ADC1BUF0);
+    int i;
+    int tempV = 0;
+    for( i = 0; i < 8; i++ ) {
+        tempV += (p[i*4]*33+511)/1023;
+    }
+
+    tempV /= 8;
+    voltage = toBcd(tempV);
+    IFS1bits.AD1IF = 0;                    // Reset AD1IF flag
+}
+
+void main(void) {
+    configureAll(); // Function to configure all (digital I/O, analog
+    // input, A/D module, timers T1 and T3, interrupts)
+    // Reset AD1IF, T1IF and T3IF flags
+    EnableInterrupts(); // Global Interrupt Enable
+    while(1);
+}
+
+void send2displays(unsigned char value) {
+    static char displayFlag = 0;
+    char display7Scodes[] = {
+            0x3F, //0
+            0x06, //1
+            0x5B, //2
+            0x4F, //3
+            0x66, //4
+            0x6D, //5
+            0x7D, //6
+            0x07, //7
+            0x7F, //8
+            0x6F, //9
+            0x77, //A
+            0x7C, //b
+            0x39, //C
+            0x5E, //d
+            0x79, //E
+            0x71  //F
+    };
+
+    unsigned char dh = display7Scodes[value >> 4];      // Get the value at 7..4
+    unsigned char dl = display7Scodes[value & 0x0F];    // Get the value at 3..0
+
+    if (displayFlag == 1) {
+        LATD = (LATD | 0x0040) & 0xFFDF;    //  activate display high & deactivate display low
+        LATB = (LATB & 0x00FF) | dh  << 8;   //  set display high value
+    } else {                      // wait
+        LATD = (LATD | 0x0020) & 0xFFBF;    //  activate display low & deactivate display high
+        LATB = (LATB & 0x80FF) | dl<< 8;   //  set display low value
+    }
+    displayFlag = !displayFlag;
+}
+
+unsigned char toBcd(unsigned char value) {
+    return ((value / 10) << 4) + (value % 10);
+}
+
+void configureAll() {
+    TRISB = (TRISB & 0x80FF) | 0xF;     // sets RB8-RB14 as output and RB0-RB as input
+    TRISD = TRISD & 0xFF9F;             // sets RD5-RD8 as output
+    LATD = (LATD & 0xFF9F) | 0x40;      // sets RD6 and RD5 to 0 then sets RD6 to 1
+    // Configure A/D module
+    TRISBbits.TRISB4 = 1;
+    AD1PCFGbits.PCFG4 = 0;
+    AD1CON1bits.SSRC = 7;
+    AD1CON1bits.CLRASAM = 1;
+    AD1CON3bits.SAMC = 16;
+    AD1CON2bits.SMPI = 8 - 1;
+    AD1CHSbits.CH0SA = 4;
+    AD1CON1bits.ON = 1;
+
+
+    // Configure interrupt system
+    IPC6bits.AD1IP = 2;  // configure priority of A/D interrupts
+    IFS1bits.AD1IF = 0;  // clear A/D interrupt flag
+    IEC1bits.AD1IE = 1;  // enable A/D interrupts
+
+    // T1 = 4Hz
+    // T3 = 100Hz
+
+    T1CONbits.TCKPS = 7; // define o Prescaler ara dividir o clock por 256
+    T3CONbits.TCKPS = 2; // define o Prescaler ara dividir o clock por 4
+
+
+    // fOut_PRESC = 20Mhz / 256 = 78125Hz
+    // PR1 = (78125Hz / 4 Hz) - 1 = 19531.25
+    PR1 = 19531; // Fout = 20MHz / (19531 + 1) = 78125/19532 ~ 3.999 Hz
+
+    // fOut_PRESC = 20Mhz / 4 = 5Mhz
+    // PR3 = (5Mhz / 100 Hz) - 1 = 49999
+    PR3 = 49999; // Fout = Fout_presc / 49999 + 1= 5*10⁶/50000 ~ 100 Hz
+
+    TMR1 = 0; // Reset timer T1 count register
+    TMR3 = 0; // Reset timer T3 count register
+
+    T1CONbits.TON = 1; // Enable timer T1 (must be the last command)
+    T3CONbits.TON = 1; // Enable timer T3 (must be the last command)
+
+
+    IFS0bits.T1IF = 0;      // Reset timer T1 interrupt flag, should be set before enabling interrupts
+    IFS0bits.T3IF = 0;      // Reset timer T1 interrupt flag, should be set before enabling interrupts
+
+    IPC1bits.T1IP = 2;      // Interrupt priority 2
+    IPC3bits.T3IP = 2;      // Interrupt priority 2
+
+    IEC0bits.T1IE = 1;      // Enable timer T1 interrupts
+    IEC0bits.T3IE = 1;      // Enable timer T1 interrupts
+}
+
+
